152c034d407a15ec3170366d78db1521a1a3d1e4
joel-emoh7 <joel.e@mblhightech.com>
2024-12-16 22:00:47 +0200

    feat: added canonical to post pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,12 +8,6 @@ import "../public/css/global.css";
 import Layout from "@/components/Layout";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const siteName = "TorConnect";
-  const siteDescription =
-    "Learn how to use the Tor Browser, explore the Dark Web safely, and access .onion sites and darknet markets. Stay updated with the latest guides and resources.";
-  const siteUrl = "https://torconnect.io";
-  const ogImage = `${siteUrl}/images/logo/logo.png`;
-
   return (
     <Auth0Provider
       clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
@@ -21,31 +15,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     >
       {/* <Preloader /> */}
       <Head>
-        <title>{`${siteName} - Explore the Dark Web with Safety and Confidence`}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content={siteDescription} />
-
-        {/* Open Graph Metadata */}
-        <meta property="og:title" content={`${siteName} - Explore the Dark Web`} />
-        <meta property="og:description" content={siteDescription} />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={siteUrl} />
-        <meta property="og:image" content={ogImage} />
-
-        {/* Twitter Card Metadata */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={`${siteName} - Explore the Dark Web`} />
-        <meta name="twitter:description" content={siteDescription} />
-        <meta name="twitter:image" content={ogImage} />
-
-        {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
 
-        {/* Canonical Link */}
-        <link rel="canonical" href={siteUrl} />
-  
       </Head>
-      
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
